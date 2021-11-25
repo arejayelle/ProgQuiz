bf6f681e70f9ef4fd9a8813de47b6be7fc1c112c
arejayelle <rebecca.j.loke@gmail.com>
2021-11-25 12:44:38 -0500

    Add coin Manager script

---
+++ Assets/Runtime/_Scripts/Managers/CoinManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+// ------------------------------------------------------------------------------
+// Quiz
+// Written by: Jun Loke 40025838
+// For COMP 376 – Fall 2021
+// -----------------------------------------------------------------------------
+//
+// Singleton to manage coins and coin display
+
+public class CoinManager : MonoBehaviour
+{
+    private static CoinManager instance;
+
+    private int numCoins = 0;
+    
+    // Get reference to UI component for updating
+    public TextMeshProUGUI coinUIText;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set up singleton
+        if (instance == null)
+            instance = this;
+        
+        coinUIText.text = $"x {numCoins}";
+    }
+
+    public void gainCoin()
+    {
+        numCoins++;
+        coinUIText.text = $"x {numCoins}";
+    }
+}
