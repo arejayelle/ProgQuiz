38620e8d3e9386c1479f39d998e45428db1f7034
arejayelle <rebecca.j.loke@gmail.com>
2021-11-26 13:16:34 -0500

    Add death type enum

---
+++ Assets/Runtime/_Scripts/DeathType.cs
@@ -0,0 +1,18 @@
+
+// ------------------------------------------------------------------------------
+// Quiz
+// Written by: Jun Loke 40025838
+// For COMP 376 – Fall 2021
+// -----------------------------------------------------------------------------
+//
+// Class to represent a sound clip
+
+namespace _Runtime
+{
+    public enum DeathType
+    {
+        Crushed,
+        LostRace,
+        Health
+    }
+}
